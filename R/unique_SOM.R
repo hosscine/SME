677cee9090196e766f61188a2d1d05e971e68998
futagamirentaro@gmail.com <futagamirentaro@gmail.com>
2018-02-26 00:33:44 +0900

    setSomでちゃんとパラメータが反映されるよう修正

--- R/unique_SOM.R
+++ R/unique_SOM.R
@@ -56,7 +56,6 @@ planesom <- R6Class(
 #'
 #' @param dim Dimension of input vector.
 #' @param weights initialize weights.
-#' @param neighbor size of the neighborhood kernel.
 #' @param alpha learning rate.
 #' @param sigma variance of the weights propagation.
 #' @param collect.stats whether rocords learning history.
@@ -65,7 +64,7 @@ planesom <- R6Class(
 #' @return Som objsect.
 #' @examples spsom <- setSom(3,"sphere")
 setSom <- function(dim, topo.mode, weights = NULL,
-                   neighbor = 1, alpha = 0.1, sigma = 1, collect.stats = F){
+                   alpha = 0.1, sigma = 1, collect.stats = F){
   if(topo.mode == "grid")
     topo <- list(c(2,7,6),c(1,7,3),c(2,7,8,9,4),c(3,9,5),c(4,9,10),c(1,7,12,11),
                  c(1,2,3,8,12,6),c(3,9,14,13,12,7),c(3,4,5,10,14,8),c(5,9,14,15),
@@ -100,7 +99,8 @@ setSom <- function(dim, topo.mode, weights = NULL,
                  c(4,13,15),c(5,14,16),c(6,15,17),c(7,16,18),c(8,17,19),c(9,18,20),c(10,11,19))
   else stop("must chose topomode from c(grid, plane, circle, sphere, torus, cylinder, cylinder.twocircle)")
 
-  som <- tpsom$new(dim = dim, topology = topo, nei = ifelse(topo.mode=="circle",2,1))
+  som <- tpsom$new(dim = dim, topology = topo, nei = ifelse(topo.mode=="circle",2,1),
+                   weights = weights, alpha = alpha, sigma = sigma, collect.stats = collect.stats)
   return(som)
 }
 
